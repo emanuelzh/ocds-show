eb22ffc4e90e3b96c19f97adac62cda0f97f4b28
Rob Redpath <rob.redpath@opendataservices.coop>
2016-12-08 19:35:11 +0000

    WIP

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/*', function(request, response) {
   console.log(request.params[0].substr(0));
   https.get(request.params[0].substr(0), (res) => {
     res.on('data', (body) => {
-      console.log(body);
+      response.send(body);
     });
   });
 });
