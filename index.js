1d7d4093b98a27a97b25a2e7ed6a9298ae35dee5
Rob Redpath <rob.redpath@opendataservices.coop>
2016-12-08 13:27:19 +0000

    h

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/*', function(request, response) {
+  response.send('hello world');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
