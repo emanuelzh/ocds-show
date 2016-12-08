e6b3c6326eb60a0124ffcbae1123a43c4382e416
Rob Redpath <rob.redpath@opendataservices.coop>
2016-12-08 18:10:26 +0000

    WIP

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ app.get('/*', function(request, response) {
 
   var file = fs.createWriteStream("file.json");
   console.log(request.params[0].substr(0))
-  var req = https.get(request.params[0].substr(0), function(response) {
-    response.pipe(file);
+  var req = https.get(request.params[0].substr(0), function(res) {
+    res.pipe(file);
   });
   var global_data = fs.readFileSync("file.json").toString();
   response.send(global_data);
